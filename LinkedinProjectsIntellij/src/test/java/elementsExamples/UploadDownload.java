1d179a18dde7b2a3c95466b3940f225b5bf29eeb
MelihDemirtel <melihcandemirtel@gmail.com>
2023-02-17 14:41:36 +0300

    uploadDownload project updated

--- LinkedinProjectsIntellij/src/test/java/elementsExamples/UploadDownload.java
+++ LinkedinProjectsIntellij/src/test/java/elementsExamples/UploadDownload.java
@@ -20,16 +20,15 @@ public class UploadDownload {
     //ELEMENTS
     static WebElement elementsButton;
     static WebElement upDwnButton;
-    static WebElement uploadButton;
     static WebElement downloadButton;
-    public WebElement uploadedFileName;
+    static WebElement uploadButton;
+    public WebElement uploadedFilePath;
 
     //OTHER
-    static String downloadUrl;
-    public String downloadedFilePath;
-    static String actualFileName;
-    static String expectedFileName = "C:\\fakepath\\sampleFile.txt";
-
+    static int milis = 1000;
+    public String downloadFilePath;
+    static String expectedFilePath = "C:\\fakepath\\sampleFile.txt";
+    static String actualFilePath;
 
     @BeforeClass
     public static void setUp(){
@@ -56,29 +55,53 @@ public class UploadDownload {
     }
     @Test
     public void test03() throws InterruptedException {
-        downloadUrl = cdriver.findElement(By.id("downloadButton")).getAttribute("href");
         downloadButton = cdriver.findElement(By.id("downloadButton"));
         downloadButton.click();
-        Thread.sleep(1000);
-        downloadedFilePath = "C:\\Users\\ext02d47194\\Downloads\\";
-        file = new File(downloadedFilePath + "sampleFile.jpeg");
-        Thread.sleep(1000);
-        if (file.exists()) {
+        Thread.sleep(milis);
+        downloadFilePath = "C:\\Users\\ext02d47194\\Downloads\\";
+        file = new File(downloadFilePath + "sampleFile.jpeg");
+        Thread.sleep(milis);
+        if (file.exists()){
             softAssert.assertEquals(file.getName(), "sampleFile.jpeg");
+            System.out.println("Dosya İndirme İşlemi Başarılıdır.");
             System.out.println("İndirilen Dosya Adı: " + file.getName());
-            System.out.println("Dosya indirme işlemi başarılıdır");
-        } else {
-            System.out.println("Dosya indirme işlemi başarısızdır");
+
+        }else {
+            System.out.println("Dosya İndirme İşlemi Başarısızdır.");
         }
+        softAssert.assertAll();
         file.delete();
     }
     @Test
     public void test04() throws InterruptedException {
         uploadButton = cdriver.findElement(By.id("uploadFile"));
         uploadButton.sendKeys("C:\\Users\\ext02d47194\\Downloads\\sampleFile.txt");
-        Thread.sleep(1000);
-        uploadedFileName = cdriver.findElement(By.id("uploadedFilePath"));
-        actualFileName = uploadedFileName.getText();
-        softAssert.assertEquals(actualFileName, expectedFileName);
+        Thread.sleep(milis);
+        uploadedFilePath = cdriver.findElement(By.id("uploadedFilePath"));
+        actualFilePath = uploadedFilePath.getText();
+        System.out.println(actualFilePath);
+        if (actualFilePath == expectedFilePath){
+            softAssert.assertEquals(actualFilePath, expectedFilePath);
+            System.out.println("Dosya Yükleme İşlemi Başarılıdır.");
+        }else {
+            System.out.println("Dosya Yükleme İşlemi Başarısızdır.");
+        }
+        softAssert.assertAll();
     }
 }
+/*
+File sınıfı, dosya ve dizin işlemlerini gerçekleştirmek için kullanılır.
+Dosya sistemi üzerinde bir dosyayı ya da dizini temsil eden bir File nesnesi oluşturulabilir.
+Bu nesne üzerinden, dosya adı, boyutu, yaratılma tarihi, son değiştirilme tarihi gibi dosya özelliklerine erişilebilir.
+Ayrıca dosyaları kopyalama, silme, taşıma, ad değiştirme gibi işlemler de yapılabilir.
+
+Public olarak downloadFilePath instance değişkeni olarak tanımlanırken, Static olarak downloadFilePath static bir değişken olarak tanımlanmıştır.
+Bir sınıfın her nesnesi için farklı değerler taşıyabilen değişkenlere "instance variable" veya "member variable" denir.
+Her bir örnek (instance) için ayrı bellek alanında saklanan bu değişkenler, sınıf içinde tanımlanır ve nesne oluşturulduğunda kullanılabilir hale gelirler.
+Instance değişkenleri, sınıf içindeki herhangi bir metotdan, constructor'dan veya block'tan erişilebilir ve değiştirilebilir.
+
+downloadFilePath instance değişkeni, her test çağrıldığında sıfırlanır ve her testin kendi downloadFilePath değerine sahip olmasını sağlar.
+Ancak, downloadFilePath static bir değişken olduğunda, testler arasında ortak bir değere sahip olur ve son testten kalma bir değerle çalışabilir.
+Bu nedenle, static örnekte, bir önceki testte indirilen dosyayı sildiğinden emin olmak için file.delete() satırını eklemek gerekir.
+Aksi takdirde, bu dosya hala varsa, dosya indirme işlemi başarılı olmayacaktır.
+ */
