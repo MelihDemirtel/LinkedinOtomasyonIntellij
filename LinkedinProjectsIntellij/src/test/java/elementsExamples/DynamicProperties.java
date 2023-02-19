ad4d31289692e0c7f415f57f18bfe52925e762b8
MelihDemirtel <melihcandemirtel@gmail.com>
2023-02-19 21:10:51 +0300

    dynamicProperties project created

---
+++ LinkedinProjectsIntellij/src/test/java/elementsExamples/DynamicProperties.java
@@ -0,0 +1,45 @@
+package elementsExamples;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
+public class DynamicProperties {
+    static WebDriver cdriver;
+    static SoftAssert softAssert;
+
+    @BeforeClass
+    public static void setUp(){
+        cdriver = new ChromeDriver();
+        softAssert = new SoftAssert();
+        cdriver.manage().window().maximize();
+        cdriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        cdriver.get("https://demoqa.com");
+    }
+    @AfterClass
+    public static void tearDown(){ cdriver.quit();
+    }
+
+    @Test
+    public void test01() {
+
+    }
+    @Test
+    public void test02() {
+
+    }
+    @Test
+    public void test03() {
+
+    }
+    @Test
+    public void test04() {
+
+    }
+}
