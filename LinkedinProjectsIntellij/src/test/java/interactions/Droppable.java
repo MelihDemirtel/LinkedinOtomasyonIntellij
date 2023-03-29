5b6763c2a357198df949ca903aa1f737bd4ad154
MelihDemirtel <melihcandemirtel@gmail.com>
2023-03-29 14:59:45 +0300

    interactions package and droppable project created

---
+++ LinkedinProjectsIntellij/src/test/java/interactions/Droppable.java
@@ -0,0 +1,42 @@
+package interactions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
+public class Droppable {
+    static WebDriver cdriver;
+    static SoftAssert softAssert;
+
+    //ELEMENTS
+    
+
+    @BeforeClass
+    public static void setUp(){
+        cdriver = new ChromeDriver();
+        softAssert = new SoftAssert();
+        cdriver.manage().window().maximize();
+        cdriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @BeforeMethod
+    public void beforeAllTests(){
+        cdriver.get("https://demoqa.com");
+
+    }
+    @AfterClass
+    public static void tearDown(){ cdriver.quit();
+    }
+    @Test
+    public void test01() {
+
+    }
+}
